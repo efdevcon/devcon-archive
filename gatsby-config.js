cbcc9b7985ed16a8555a9a2c7efdf32b8bce38e4
Paul Jickling <pjickling@gmail.com>
2020-01-25 13:22:33 -0800

    Metadata changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Devcon Archive`,
+    description: `The Devcon Archive page`,
+    author: `@pauljickling, @ev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
