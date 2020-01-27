ab12d24d091d444a5c29fb48518c067719f22c08
Paul Jickling <pjickling@gmail.com>
2020-01-27 13:39:53 -0800

    Added routes for each Devcon page

---
+++ src/pages/devcon-0/supporters.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../../index.css";
+
+const Supporters = () => (
+  <p>Hello supporters</p>
+);
+
+export default Supporters;
---
+++ src/pages/devcon-4/details.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../index.css";
+
+const Details = () => (
+  <p>Hello details</p>
+);
+
+export default Details;
