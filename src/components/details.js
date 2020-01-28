0abb1c93bcffe9bdf61c82b4f72e768f4f1bc6eb
Paul Jickling <pjickling@gmail.com>
2020-01-28 15:35:55 -0800

    Added navbar component for devcon pages

--- src/components/details.js
+++ src/components/details.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+/* CSS */
 import "../index.css";
 
-const Details = () => (
-  <p>Hello details</p>
+/* Components */
+import Footer from "./footer";
+import Header from "./header";
+import Navbar from "./navbar";
+
+const Details = (props) => (
+  <div>
+    
+    <Header />
+
+    <div>Details Hero Component</div>
+    {/* TODO remove hard encoding */}
+    <Navbar devcon="devcon-0" />
+    <div>Details About Component</div>
+    <div>Featured Videos Component</div>
+    <div>Featured Supporters Component</div>
+    <Footer />
+  </div>
 );
 
 export default Details;
---
+++ src/components/navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "gatsby";
+
+/* CSS */
+import css from "./navbar.module.css";
+import "../index.css";
+
+const Navbar = props => (
+  <nav>
+    <ul className={css.navbar}>
+
+      <li className={css.navitem}>
+        <Link className={css.navlink} to={`${props.devcon}/details`}>
+          Details
+        </Link>
+      </li>
+
+      <li className={css.navitem}>
+        <Link className={css.navlink} to={`${props.devcon}/videos`}>
+          Videos
+        </Link>
+      </li>
+
+      <li className={css.navitem}>
+        <Link className={css.navlink} to={`${props.devcon}/supporters`}>
+          Supporters
+        </Link>
+      </li>
+
+    </ul>
+  </nav>
+);
+
+export default Navbar;
