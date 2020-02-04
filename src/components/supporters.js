165a3a89e10a231ad4e44b6facb5e72260b9f7a0
Paul Jickling <pjickling@gmail.com>
2020-02-04 13:25:10 -0800

    Added (currently unused props to supporters component page

--- src/components/supporters.js
+++ src/components/supporters.js
@@ -10,17 +10,24 @@ import Footer from "./footer";
 import Header from "./header";
 import Navbar from "./navbar";
 
-const Supporters = () => (
-  <div>
-    <Header />
-    <ArchiveHero
-      number="0"
-      location="London, United Kingdom"
-      date="Nov 9 - 13, 2015"
-    />
-    <Navbar devcon="devcon-0" />
-    <Footer />
-  </div>
-);
+const Supporters = (props) => {
+ 
+  return (
+    <div>
+      <Header />
+      <ArchiveHero
+        number="0"
+        location="London, United Kingdom"
+        date="Nov 9 - 13, 2015"
+      />
+      <Navbar devcon="devcon-0" />
+      <main>
+        <h3>{props.support1}</h3>
+        <div className={props.logos}></div>
+      </main>
+      <Footer />
+    </div>
+  )
+};
 
 export default Supporters;
