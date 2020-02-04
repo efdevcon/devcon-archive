d8798fc375c95fc46fe7c8aac25a3887dc68173a
Paul Jickling <pjickling@gmail.com>
2020-02-03 16:36:12 -0800

    Updated Videos component

--- src/components/videos.js
+++ src/components/videos.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+/* CSS */
 import "../index.css";
 
+/* Components */
+import ArchiveHero from "./archive_hero";
+import Footer from "./footer";
+import Header from "./header";
+import Navbar from "./navbar";
+
+
 const Videos = () => (
-  <p>Hello videos</p>
+  <div>
+    <Header />
+    <ArchiveHero
+      numer="0"
+      location="London, United Kingdom"
+      date="Nov 9 - 13, 2015"
+    />
+    <Navbar devcon="0" />
+    <Footer />
+  </div>
 );
 
 export default Videos;
