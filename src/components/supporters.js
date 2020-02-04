9906af6acca296a83c5da4d94d3a59da81ae5a49
Paul Jickling <pjickling@gmail.com>
2020-02-03 16:54:51 -0800

    Added skeleton of supporters page

--- src/components/supporters.js
+++ src/components/supporters.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+/* CSS */
+import css from "./supporters.module.css";
 import "../index.css";
 
+/* Components */
+import ArchiveHero from "./archive_hero";
+import Footer from "./footer";
+import Header from "./header";
+import Navbar from "./navbar";
+
 const Supporters = () => (
-  <p>Hello supporters</p>
+  <div>
+    <Header />
+    <ArchiveHero
+      number="0"
+      location="London, United Kingdom"
+      date="Nov 9 - 13, 2015"
+    />
+    <Navbar devcon="0" />
+    <Footer />
+  </div>
 );
 
 export default Supporters;
