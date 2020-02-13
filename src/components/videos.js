6b2eefcf3c6451120a317f2fba5bcc6b0c3fdda5
Paul Jickling <pjickling@gmail.com>
2020-02-13 00:12:57 -0600

    Video page now loads yaml data (although youtube embed not yet working)

--- src/components/videos.js
+++ src/components/videos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 /* CSS */
 import css from "./videos.module.css";
@@ -9,23 +10,59 @@ import ArchiveHero from "./archive_hero";
 import Footer from "./footer";
 import Header from "./header";
 import Navbar from "./navbar";
+import VideoCard from "./video_card";
 
-const Videos = props => (
-  <div>
-    <Header />
-    <ArchiveHero
-      number={props.number}
-      location={props.location}
-      date={props.date}
-    />
-    <main>
-      <div className={css.filters}>
-        All | Main Stage | Second Stage | Breakout Rooms
-      </div>
-    </main>
-    <Navbar devcon={`devcon-${props.number}`} />
-    <Footer />
-  </div>
-);
+const Videos = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      allVideosYaml {
+        edges {
+          node {
+            devcon
+            data {
+              url
+              title
+              speakers
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <div>
+      <Header />
+      <ArchiveHero
+        number={props.number}
+        location={props.location}
+        date={props.date}
+      />
+      <Navbar devcon={`devcon-${props.number}`} />
+      <main>
+        <div className={css.filters}>
+          All | Main Stage | Second Stage | Breakout Rooms
+        </div>
+        <div>
+          {data.allVideosYaml.edges.map((edge, index) => (
+            <div key={index}>
+              <ul>
+                {edge.node.devcon === `devcon-${props.number}` &&
+                  edge.node.data.map(video => (
+                    <VideoCard
+                      src={video.url}
+                      title={video.title}
+                      speakers={video.speakers}
+                    />
+                  ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Videos;
