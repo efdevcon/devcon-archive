e285cdb96c09900c6298784a6decbf8004769e7c
Everton Fraga <ev@ethereum.org>
2020-02-28 16:21:31 -0500

    Initial hero of devcon sections

--- src/components/archive_hero.js
+++ src/components/archive_hero.js
@@ -1,21 +1,29 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 /* CSS */
 import css from "./archive_hero.module.css";
 import "../index.css";
 
+/* HTML */
+import BackgroundImage from 'gatsby-background-image'
+
 /* Components */
 import Logo from "./logo";
 
-const ArchiveHero = props => (
-  <div className={css.archiveHero}>
-    <Logo />
-    <div>
-      <h1>Devcon {props.number}</h1>
-      <h2>{props.location}</h2>
-      <h4>{props.date}</h4>
-    </div>
-  </div>
-);
+const ArchiveHero = props => {
+  const data = useStaticQuery(graphql`query { file(relativePath: { eq: "devcon0-background.png" }) { childImageSharp { fluid(maxWidth: 1200) { ...GatsbyImageSharpFluid } } } }`)
+  
+  return (
+    <BackgroundImage fluid={data.file.childImageSharp.fluid} className={css.archiveHero}>
+      <Logo />
+      <div>
+        <h1>Devcon {props.number}</h1>
+        <h2>{props.location}</h2>
+        <h4>{props.date}</h4>
+      </div>
+    </BackgroundImage>
+  );
+}
 
 export default ArchiveHero;
