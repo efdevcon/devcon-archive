26f4379751b1820f5be10554729ead216b840990
Everton Fraga <ev@ethereum.org>
2020-02-28 14:55:00 -0500

    Improving homepage hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,26 +1,45 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 /* CSS */
 import css from "./hero.module.css";
 import "../index.css";
 
-/* Components */
-import Logo from "./logo";
+/* HTML */
+import BackgroundImage from 'gatsby-background-image'
 
-const Hero = () => (
-  <div className={css.hero}>
-    <Logo />
-    <h2>Devcon</h2>
-    <p className={css.heroText}>
-      Annual conference for all Ethereum developers, researchers, thinkers, and
-      makers
-    </p>
-    <p className={css.callToAction}>
-      <a className={css.callToActionLink} href="#devcon-about">
-        Read More
-      </a>
-    </p>
-  </div>
-);
+/* Components */
+import LogoDevcon from "./logo-devcon";
+
+
+
+const Hero = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "archive-background.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <BackgroundImage fluid={data.file.childImageSharp.fluid} className={css.hero} Tag="section">
+      <h1><LogoDevcon /></h1>
+      <p className={css.heroText}>
+        Annual conference for all Ethereum developers, researchers, thinkers, and
+        makers
+      </p>
+      <p className={css.callToAction}>
+        <a className={css.callToActionLink} href="#devcon-about">
+          Read More
+        </a>
+      </p>
+    </BackgroundImage>
+  )
+};
 
 export default Hero;
