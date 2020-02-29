29412e3213509946e519a9fda9bae7b21082498c
Paul Jickling <pjickling@gmail.com>
2020-02-28 16:58:04 -0800

    Changed location props for consistency

--- src/pages/devcon-0/supporters.js
+++ src/pages/devcon-0/supporters.js
@@ -9,11 +9,7 @@ import Supporters from "../../components/supporters";
 /* This page is for routing purposes only. To change page layout please edit the imported Supporters component */
 
 const Devcon0Supporters = () => (
-  <Supporters
-    number="0"
-    location="Kreuzberg, Berlin"
-    date="Nov 24 - 28, 2014"
-  />
+  <Supporters number="0" location="Berlin, Germany" date="Nov 24 - 28, 2014" />
 );
 
 export default Devcon0Supporters;
