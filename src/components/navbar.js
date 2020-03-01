1aa75a366ff30c1ce4da33313184863451bed050
Paul Jickling <pjickling@gmail.com>
2020-03-01 13:02:51 -0800

    Testing out color variables

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,28 +5,30 @@ import { Link } from "gatsby";
 import css from "./navbar.module.css";
 import "../index.css";
 
-const Navbar = props => (
-  <nav>
-    <ul className={css.navbar}>
-      <li className={css.navitem}>
-        <Link className={css.navlink} to={`${props.devcon}/details`}>
-          Details
-        </Link>
-      </li>
+const Navbar = props => {
+  return (
+    <nav>
+      <ul className={css.navbar}>
+        <li className={css.navitem}>
+          <Link className={css.navlink} to={`${props.devcon}/details`}>
+            Details
+          </Link>
+        </li>
 
-      <li className={css.navitem}>
-        <Link className={css.navlink} to={`${props.devcon}/videos`}>
-          Videos
-        </Link>
-      </li>
+        <li className={css.navitem}>
+          <Link className={css.navlink} to={`${props.devcon}/videos`}>
+            Videos
+          </Link>
+        </li>
 
-      <li className={css.navitem}>
+        {/* <li className={css.navitem}>
         <Link className={css.navlink} to={`${props.devcon}/supporters`}>
           Supporters
         </Link>
-      </li>
-    </ul>
-  </nav>
-);
+      </li> */}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
