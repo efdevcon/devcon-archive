9b8b8b842eaadff808132bad95ea74beffc9ae32
Paul Jickling <pjickling@gmail.com>
2020-03-02 15:26:49 -0800

    Added Devcon Headers to details page

--- src/components/details_about.js
+++ src/components/details_about.js
@@ -8,7 +8,7 @@ const DetailsAbout = props => (
   <div className={css.about}>
     <div className={css.aboutColumn}>TODO: Image goes here</div>
     <div className={css.aboutColumn}>
-      <h2>Devcon Header Text</h2>
+      <h2 className={css.header}>{props.header}</h2>
       <p className={css.aboutText}>{props.text}</p>
     </div>
   </div>
