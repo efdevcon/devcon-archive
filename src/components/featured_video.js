eab3d7fc0a6fa67137abd1956be8d19125e421a5
Paul Jickling <pjickling@gmail.com>
2020-03-03 14:53:34 -0800

    WIP: featured video UI improvements

--- src/components/featured_video.js
+++ src/components/featured_video.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 
 /* CSS */
 import css from "./featured_video.module.css";
 import "../index.css";
 
 const FeaturedVideo = props => {
+
   return (
     <div className={css.featuredVideo}>
-      <h2>Featured Videos</h2>
+      <h2 className={css.header}>Featured Videos</h2>
       <div className={css.videos}>
         <div className={css.video}>Video 1</div>
         <div className={css.video}>Video 2</div>
         <div className={css.video}>Video 3</div>
       </div>
       <p>
-        <Link to={`${props.devcon}/videos`}>Check Out More Videos</Link>
+        <Link className={css.videoLinks} to={`${props.devcon}/videos`}>
+          Check Out More Videos
+        </Link>
       </p>
     </div>
   );
