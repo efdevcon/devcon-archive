c2d018e83b0a7461e5dbb0c0540cb6bdb3bcebb9
Paul Jickling <pjickling@gmail.com>
2020-03-04 10:00:39 -0800

    Updated footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,62 @@
 import React from "react";
+import Img from "gatsby-image";
+import { graphql, useStaticQuery } from "gatsby";
+/* CSS */
 import css from "./footer.module.css";
 import "../index.css";
 
-const Footer = () => (
-  <div className={css.footer}>
-    <p className={css.copyright}>© Ethereum Foundation, 2020</p>
-    <p className={css.link}>
-      <a className={css.footerLink} href="https://devcon.org">
-        Devcon
-      </a>
-    </p>
-  </div>
-);
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(
+        childImageSharp: { fixed: { originalName: { eq: "twitter_icon.png" } } }
+      ) {
+        childImageSharp {
+          fixed(width: 32, height: 26) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <div className={css.footer}>
+      <div className={css.topRow}>
+        <p className={css.link}>
+          <a className={css.footerLink} href="https://devcon.org">
+            Devcon
+          </a>
+        </p>
+        <div className={css.divider}>|</div>
+        <p className={css.link}>
+          <a
+            className={css.footerLink}
+            href="https://blog.ethereum.org/category/devcon/"
+          >
+            Blog
+          </a>
+        </p>
+        <div className={css.divider}>|</div>
+        <div className={css.link}>
+          <a className={css.footerLink} href="https://twitter.com/ethereum">
+            <Img
+              className={css.twitterIcon}
+              fixed={data.file.childImageSharp.fixed}
+              alt="Twitter link"
+            />
+          </a>
+        </div>
+        <p className={css.link}>
+          <a className={css.footerLink} href="https://twitter.com/ethereum">
+            Twitter
+          </a>
+        </p>
+      </div>
+      <div className={css.bottomRow}>
+        <p className={css.copyright}>© Ethereum Foundation, 2020</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
