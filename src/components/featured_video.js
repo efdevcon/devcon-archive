1f967fec32f5de87d0e02ee2dc5bc9fa4bf62e5b
Paul Jickling <pjickling@gmail.com>
2020-03-03 16:48:07 -0800

    MVP of featured video component

--- src/components/featured_video.js
+++ src/components/featured_video.js
@@ -6,15 +6,50 @@ import css from "./featured_video.module.css";
 import "../index.css";
 
 const FeaturedVideo = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      allVideosYaml {
+        edges {
+          node {
+            devcon
+            data {
+              title
+              featured
+              speakers
+              url
+            }
+          }
+        }
+      }
+    }
+  `);
+  const videoData = data.allVideosYaml.edges[props.devconNum];
+  const featuredData = videoData.node.data.filter(video => !!video.featured);
+
   return (
     <div className={css.featuredVideo}>
       <h2 className={css.header}>Featured Videos</h2>
-      <div className={css.videos}>
-        <div className={css.video}>Video 1</div>
-        <div className={css.video}>Video 2</div>
-        <div className={css.video}>Video 3</div>
+      <div className={css.videoGrid}>
+        {featuredData.map((video, index) => (
+          <div className={css.videoCard} key={index}>
+            <div className={css.iframeContainer}>
+              <iframe
+                frameBorder="0"
+                src={video.url}
+                title={video.title}
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                webkitallowfullscreen="true"
+                mozallowfullscreen="true"
+                allowFullScreen
+              />
+            </div>
+            <p className={css.title}>{video.title}</p>
+            <p className={css.speakers}>By {video.speakers}</p>
+          </div>
+        ))}
       </div>
-      <p>
+
+      <p className={css.videoLinksContainer}>
         <Link className={css.videoLinks} to={`${props.devcon}/videos`}>
           Check Out More Videos
         </Link>
