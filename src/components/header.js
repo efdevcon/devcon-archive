064f54be170ea83b0b961c2d2e3914c5cdf6ed1d
Paul Jickling <pjickling@gmail.com>
2020-03-04 16:25:49 -0800

    Added EF logo to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,54 @@
 import React from "react";
-import { Link } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 
 /* CSS */
 import css from "./header.module.css";
 import "../index.css";
 
-const Header = props => (
-  <header className={css.header}>
-    <div className={css.headerItem}>
-      <a
-        className={`${css.headerLink} ${
-          props.color === "white" ? css.colorWhite : css.colorBlack
-        }`}
-        href="https://devcon.org"
-      >
-        Devcon.org
-      </a>
-    </div>
-    <div className={css.headerItem}>
-      <a
-        className={`${css.headerLink} ${
-          props.color === "white" ? css.colorWhite : css.colorBlack
-        }`}
-        href="https://blog.ethereum.org/category/devcon/"
-      >
-        Blog
-      </a>
-    </div>
-  </header>
-);
+const Header = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "ethereum_foundation_logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <header className={css.header}>
+      <div className={css.logoContainer}>
+        <Img
+          className={css.efLogo}
+          fluid={data.file.childImageSharp.fluid}
+          alt="Ethereum Foundation Logo"
+        />
+      </div>
+      <div className={css.headerItem}>
+        <a
+          className={`${css.headerLink} ${
+            props.color === "white" ? css.colorWhite : css.colorBlack
+          }`}
+          href="https://devcon.org"
+        >
+          Devcon.org
+        </a>
+      </div>
+      <div className={css.headerItem}>
+        <a
+          className={`${css.headerLink} ${
+            props.color === "white" ? css.colorWhite : css.colorBlack
+          }`}
+          href="https://blog.ethereum.org/category/devcon/"
+        >
+          Blog
+        </a>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
