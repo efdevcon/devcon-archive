4003255e18206dfcdaad84a87b85eae5f812cfb3
Everton Fraga <ev@ethereum.org>
2020-03-10 15:10:47 -0400

    Section logos

--- src/components/archive.js
+++ src/components/archive.js
@@ -23,7 +23,7 @@ const Archive = props => {
         edges {
           node {
             fluid(maxWidth: 1200) {
-              ...GatsbyImageSharpFluid
+              ...GatsbyImageSharpFluid_tracedSVG
               originalName
             }
           }
@@ -48,45 +48,32 @@ const Archive = props => {
 
       <div className={css.tint}></div>
 
-      <div className={css.archiveCol}>
-        <Logo />
-      </div>
+      <div className={css.row}>
 
-      <div className={css.archiveCol}>
-        <h1>{props.header}</h1>
-        <h3>{props.location}</h3>
-        <h4>{props.date}</h4>
-      </div>
+        <div className={css.col}>
+          <Logo number={props.number} />
+        </div>
 
-      <div className={css.archiveCol}>
-        <p className={css.archiveText}>{props.text}</p>
-        <ul className={`${css.archiveLinks} ${gradientClass}`}>
-          <li>
-            <a
-              className={`${hoverClass}`}
-              href={`./devcon-${props.number}/details`}
-            >
-              Details
-            </a>
-          </li>
-          <li>|</li>
-          <li>
-            <a
-              className={`${hoverClass}`}
-              href={`./devcon-${props.number}/videos`}
-            >
-              {" "}
-              Videos
-            </a>
-          </li>
-          {/*
-          <Link
-            className={css.archiveLink}
-            to={`devcon-${props.number}/supporters`}
-          >
-            Supporters
-          </Link> */}
-        </ul>
+        <div className={css.col}>
+          <p className={css.archiveText}>{props.text}</p>
+          <ul className={`${css.archiveLinks} ${gradientClass}`}>
+            <li>
+              <a className={`${hoverClass}`}
+                 href={`./devcon-${props.number}/details`}>
+                Details
+              </a>
+            </li>
+            <li>|</li>
+            <li>
+              <a
+                className={`${hoverClass}`}
+                href={`./devcon-${props.number}/videos`}>
+                Videos
+              </a>
+            </li>
+            {/*<Link className={css.archiveLink} to={`devcon-${props.number}/supporters`}>Supporters</Link> */}
+          </ul>
+        </div>
       </div>
     </BackgroundImage>
   );
--- src/components/logo.js
+++ src/components/logo.js
@@ -6,10 +6,46 @@ import Img from "gatsby-image";
 import css from "./logo.module.css";
 import "../index.css";
 
-const Logo = () => {
+const Logo = props => {
+
   const data = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "ethereum-diamond.png" }) {
+      dc0logo:file(relativePath: { eq: "dc0-logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      dc1logo:file(relativePath: { eq: "dc1-logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      dc2logo:file(relativePath: { eq: "dc2-logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      dc3logo:file(relativePath: { eq: "dc3-logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      dc4logo:file(relativePath: { eq: "dc4-logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      dc5logo:file(relativePath: { eq: "dc5-logo.png" }) {
         childImageSharp {
           fluid(maxWidth: 600) {
             ...GatsbyImageSharpFluid
@@ -18,11 +54,12 @@ const Logo = () => {
       }
     }
   `);
+  console.log('DATA', data);
   return (
     <div className={css.logoContainer}>
       <Img
         className={css.logo}
-        fluid={data.file.childImageSharp.fluid}
+        fluid={data[`dc${props.number}logo`].childImageSharp.fluid}
         alt="Ethereum Logo"
       />
     </div>
