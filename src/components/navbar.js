927715df8fae3a64303fb4b4e6fd53298ff9bf02
Paul Jickling <pjickling@gmail.com>
2020-03-10 10:26:28 -0700

    Added home link to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,6 +13,11 @@ const Navbar = props => {
   return (
     <nav className={`${backgroundClass} ${highlightClass}`}>
       <ul className={css.navbar}>
+        <li className={css.navItem}>
+          <Link className={css.navLink} to='/'>
+            Home
+          </Link>
+        </li>
         <li className={css.navitem}>
           <Link className={css.navlink} to={`${props.devcon}/details`}>
             Details
