3469ec8a5e69a4fd72ea1dff856b2fc955c0cbd2
Paul Jickling <pjickling@gmail.com>
2020-03-10 11:38:24 -0700

    Made copies of details pages as index pages for each route

---
+++ src/pages/devcon-0/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/* CSS */
+import "../../index.css";
+
+/* Components */
+import Details from "../../components/details";
+
+/* This page is for routing purposes only. To change page layout please edit the imported Details component */
+
+const Devcon0Details = () => (
+  <Details number="0" location="Berlin, Germany" date="Nov 24 - 28, 2014" />
+);
+
+export default Devcon0Details;
---
+++ src/pages/devcon-3/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/* CSS */
+import "../../index.css";
+
+/* Components */
+import Details from "../../components/details";
+
+/* This page is for routing purposes only. To change page layout please edit the imported Details component */
+
+const Devcon3Details = () => (
+  <Details number="3" location="Cancún, Mexico" date="Nov 1 - 4, 2017" />
+);
+
+export default Devcon3Details;
