7dc185f2bbd6a32f3baec985e5b21f5bd8a45354
Paul Jickling <pjickling@gmail.com>
2020-03-16 11:54:13 -0700

    Removed console.log statement

--- src/components/logo.js
+++ src/components/logo.js
@@ -7,7 +7,6 @@ import css from "./logo.module.css";
 import "../index.css";
 
 const Logo = props => {
-
   const data = useStaticQuery(graphql`
     fragment Logo on File {
       childImageSharp {
@@ -18,24 +17,48 @@ const Logo = props => {
     }
 
     query {
-      DC0Logo: file(relativePath: { eq: "dc0-logo@2x.png" }) { ...Logo }
-      DC1Logo: file(relativePath: { eq: "dc1-logo@2x.png" }) { ...Logo }
-      DC2Logo: file(relativePath: { eq: "dc2-logo@2x.png" }) { ...Logo }
-      DC3Logo: file(relativePath: { eq: "dc3-logo@2x.png" }) { ...Logo }
-      DC4Logo: file(relativePath: { eq: "dc4-logo@2x.png" }) { ...Logo }
-      DC5Logo: file(relativePath: { eq: "dc5-logo@2x.png" }) { ...Logo }
-      DC0LogoWhite: file(relativePath: { eq: "dc0-logo-white@2x.png" }) { ...Logo }
-      DC1LogoWhite: file(relativePath: { eq: "dc1-logo-white@2x.png" }) { ...Logo }
-      DC2LogoWhite: file(relativePath: { eq: "dc2-logo-white@2x.png" }) { ...Logo }
-      DC3LogoWhite: file(relativePath: { eq: "dc3-logo-white@2x.png" }) { ...Logo }
-      DC4LogoWhite: file(relativePath: { eq: "dc4-logo-white@2x.png" }) { ...Logo }
-      DC5LogoWhite: file(relativePath: { eq: "dc5-logo-white@2x.png" }) { ...Logo }
+      DC0Logo: file(relativePath: { eq: "dc0-logo@2x.png" }) {
+        ...Logo
+      }
+      DC1Logo: file(relativePath: { eq: "dc1-logo@2x.png" }) {
+        ...Logo
+      }
+      DC2Logo: file(relativePath: { eq: "dc2-logo@2x.png" }) {
+        ...Logo
+      }
+      DC3Logo: file(relativePath: { eq: "dc3-logo@2x.png" }) {
+        ...Logo
+      }
+      DC4Logo: file(relativePath: { eq: "dc4-logo@2x.png" }) {
+        ...Logo
+      }
+      DC5Logo: file(relativePath: { eq: "dc5-logo@2x.png" }) {
+        ...Logo
+      }
+      DC0LogoWhite: file(relativePath: { eq: "dc0-logo-white@2x.png" }) {
+        ...Logo
+      }
+      DC1LogoWhite: file(relativePath: { eq: "dc1-logo-white@2x.png" }) {
+        ...Logo
+      }
+      DC2LogoWhite: file(relativePath: { eq: "dc2-logo-white@2x.png" }) {
+        ...Logo
+      }
+      DC3LogoWhite: file(relativePath: { eq: "dc3-logo-white@2x.png" }) {
+        ...Logo
+      }
+      DC4LogoWhite: file(relativePath: { eq: "dc4-logo-white@2x.png" }) {
+        ...Logo
+      }
+      DC5LogoWhite: file(relativePath: { eq: "dc5-logo-white@2x.png" }) {
+        ...Logo
+      }
     }
   `);
-  console.log('DATA', data);
 
-  let imageKey = ('white' in props)? `DC${props.number}LogoWhite` : `DC${props.number}Logo`
-  
+  let imageKey =
+    "white" in props ? `DC${props.number}LogoWhite` : `DC${props.number}Logo`;
+
   return (
     <div className={css.logoContainer}>
       <Img
