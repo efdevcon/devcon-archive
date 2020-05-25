f7a43af1c6e58b2de81d6aefbe713c2c3d210370
Paul Jickling <pjickling@gmail.com>
2020-05-25 11:56:11 -0700

    Added fog background and window width helper function

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
 /* CSS */
@@ -12,9 +12,22 @@ import BackgroundImage from "gatsby-background-image";
 // import Button from "./button";
 import LogoDevcon from "./logo-devcon";
 
+/* Helper Function */
+import useWindowWidth from "../helper/useWindowWidth";
+
 const Hero = () => {
-  const { triangles, city } = useStaticQuery(graphql`
+  const width = useWindowWidth();
+  const isMobile = width <= 768;
+
+  const { fog, triangles, city } = useStaticQuery(graphql`
     query {
+      fog: file(relativePath: { eq: "background-fog.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
       triangles: file(
         relativePath: { eq: "devcon_archive_hero_background.jpeg" }
       ) {
@@ -35,9 +48,17 @@ const Hero = () => {
   `);
 
   const backgroundFluidImageStack = [
-    city.childImageSharp.fluid, 
+    fog.childImageSharp.fluid,
+    city.childImageSharp.fluid,
     triangles.childImageSharp.fluid,
   ];
+
+  const getBackgroundPosition = isMobile
+    ? "center 10%, bottom center, 100% bottom"
+    : "bottom 100%, top center, bottom";
+  const getBackgroundSize = isMobile
+    ? "cover, cover, cover"
+    : "cover, 140%, 100%";
 
   return (
     <BackgroundImage
@@ -45,10 +66,10 @@ const Hero = () => {
       className={css.hero}
       Tag="section"
       style={{
-        /* city, triangles */
-        backgroundRepeat: 'no-repeat, no-repeat',
-        backgroundPosition: 'bottom, top',
-        backgroundSize: 'contain, cover',
+        /* fog, triangles, city */
+        backgroundRepeat: "no-repeat, no-repeat, no-repeat",
+        backgroundPosition: getBackgroundPosition,
+        backgroundSize: getBackgroundSize,
       }}
     >
       <div className={css.logoDivider}>
