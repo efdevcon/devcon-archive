835b27f00256109b7b3a23df7e2d2dfc314b1138
Paul Jickling <pjickling@gmail.com>
2020-05-27 09:58:54 -0700

    Updated hero background image and logo

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
 /* CSS */
@@ -9,37 +9,16 @@ import "../index.css";
 import BackgroundImage from "gatsby-background-image";
 
 /* Components */
-// import Button from "./button";
 import LogoDevcon from "./logo-devcon";
 
-/* Helper Function */
-import useWindowWidth from "../helper/useWindowWidth";
-
 const Hero = () => {
-  const width = useWindowWidth();
-  const isMobile = width <= 768;
-
-  const { fog, triangles, city } = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
-      fog: file(relativePath: { eq: "background-fog.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      triangles: file(
-        relativePath: { eq: "devcon_archive_hero_background.jpeg" }
+      file(
+        relativePath: { eq: "devcon-bogota-section-design-background.jpg" }
       ) {
         childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      city: file(relativePath: { eq: "background-landscape@2x.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
+          fluid {
             ...GatsbyImageSharpFluid
           }
         }
@@ -47,35 +26,20 @@ const Hero = () => {
     }
   `);
 
-  const backgroundFluidImageStack = [
-    fog.childImageSharp.fluid,
-    city.childImageSharp.fluid,
-    triangles.childImageSharp.fluid,
-  ];
-
-  const getBackgroundPosition = isMobile
-    ? `50% top, bottom center, 100% bottom`
-    : "bottom 100%, top center, bottom";
-  const getBackgroundSize = isMobile
-    ? "200%, cover, cover"
-    : "cover, 140%, 100%";
-
   return (
     <BackgroundImage
-      fluid={backgroundFluidImageStack}
+      fluid={data.file.childImageSharp.fluid}
       className={css.hero}
       Tag="section"
       style={{
-        /* fog, triangles, city */
-        backgroundRepeat: "no-repeat, no-repeat, no-repeat",
-        backgroundPosition: getBackgroundPosition,
-        backgroundSize: getBackgroundSize,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "0 100%",
+        backgroundSize: "cover",
       }}
     >
       <div className={css.logoDivider}>
         <LogoDevcon />
       </div>
-      <h3 className={css.announcement}>COLOMBIA 2021</h3>
       <p className={css.heroText}>
         The annual conference for all Ethereum developers, researchers,
         thinkers, and makers.
@@ -83,12 +47,6 @@ const Hero = () => {
       <p className={css.heroText}>
         See recent updates on our <a href="#">Devcon blog</a>
       </p>
-
-      {/* <div className={css.buttonContainer}>
-        <Button devconNum="default" link="https://devcon.org">
-          Devcon6 Info »{" "}
-        </Button>
-      </div> */}
     </BackgroundImage>
   );
 };
--- src/components/logo-devcon.js
+++ src/components/logo-devcon.js
@@ -1,17 +1,28 @@
 import React from "react";
-
-import Logo from "../images/dc6_logo.svg";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 /* CSS */
 import css from "./logo-devcon.module.css";
 import "../index.css";
 
 const LogoDevcon = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "devcon-bogota-logo-for-archive.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 800) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `);
   return (
     <div className={css.logoContainer}>
-      <img
+      <Img
         className={css.logo}
-        src={Logo}
+        fluid={data.file.childImageSharp.fluid}
         alt="Devcon Logo"
       />
     </div>
