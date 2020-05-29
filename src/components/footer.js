3abc6919e14dc9f5c4131ebbcc295e76bf82080b
Paul Jickling <pjickling@gmail.com>
2020-05-29 11:09:29 -0700

    Footer improvements

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,55 +7,55 @@ import "../index.css";
 const Footer = () => (
   <footer className={css.footer}>
     {/* EF Links */}
-    <ul className={css.row}>
-      <li className={css.link}>
+    <div className={css.row}>
+      <div className={css.link}>
         <a
           className={css.footerLink}
           href="https://blog.ethereum.org/category/devcon/"
         >
           Blog
         </a>
-      </li>
+      </div>
 
-      <li className={css.link}>
+      <div className={css.link}>
         <a className={`${css.footerLink}`} href="https://twitter.com/EFDevcon">
           Twitter
         </a>
-      </li>
+      </div>
 
-      <li className={css.link}>
+      <div className={css.link}>
         <a className={css.footerLink} href="https://ethereum.foundation">
-          EF
+          Ethereum Foundation
         </a>
-      </li>
-    </ul>
+      </div>
+
 
     {/* Legal */}
-    <ul className={css.row}>
-      <li className={css.link}>
+
+      <div className={css.link}>
         <a
           className={css.footerLink}
           href="https://ethereum.org/cookie-policy/"
         >
           Cookie Policy
         </a>
-      </li>
+      </div>
 
-      <li className={css.link}>
+      <div className={css.link}>
         <a
           className={css.footerLink}
           href="https://ethereum.org/privacy-policy/"
         >
           Privacy Policy
         </a>
-      </li>
+      </div>
 
-      <li className={css.link}>
+      <div className={css.link}>
         <a className={css.footerLink} href="https://ethereum.org/terms-of-use/">
           Terms of Use
         </a>
-      </li>
-    </ul>
+      </div>
+    </div>
 
     {/* Copyright */}
     <p className={css.copyright}>© Ethereum Foundation, 2020</p>
